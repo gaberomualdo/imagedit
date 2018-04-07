5c519e1882a7949c852205a1020d797445621aa5
Fred <36894883+xtrp@users.noreply.github.com>
2018-04-07 11:56:26 +0100

    Finished Javascript and HTML

--- script.js
+++ script.js
@@ -2,5 +2,37 @@ function openImage(src){
 	document.getElementById("imgSrcForm").setAttribute("style","display:none;");
 	document.getElementById("mainImageEditArea").removeAttribute("style");
 
-	document.getElementById("mainImage").setAttribute("src",src);
+	document.getElementById("mainImageBackgroundImage").innerHTML = "#mainImage {background-image: url(" + src + ");}";
+}
+
+var editValues = [];
+var editNames = [];
+var editUnits = [];
+
+window.onload = function(){
+	var allEditInputs = document.getElementById("imageEditing").getElementsByTagName("input");
+	for(var i = 0;i < allEditInputs.length;i++){
+		allEditInputs[i].setAttribute("id",i);
+		editNames.push(allEditInputs[i].getAttribute("class"));
+		editUnits.push(allEditInputs[i].getAttribute("unit"));
+		if(allEditInputs[i].value == ""){
+			allEditInputs[i].value = 0;
+		}
+		editValues.push(allEditInputs[i].value);
+		allEditInputs[i].oninput = function(){
+			if(this.value == ""){
+				this.value = 0;
+			}
+			editValues[this.getAttribute("id")] = this.value;
+			updateFilters();
+		}
+	}
+}
+function updateFilters(){
+	var cssCode = "#mainImage {filter: ";
+	editValues.forEach(function(item,index){
+		cssCode += editNames[index] + "(" + item + editUnits[index] + ") ";
+	});
+	cssCode += ";}";
+	document.getElementById("mainImageEdits").innerHTML = cssCode;
 }
